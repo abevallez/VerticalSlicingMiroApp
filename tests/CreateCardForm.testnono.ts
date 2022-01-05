f184dca0e49b526101097295af5a6e1674c46b25
Abraham Vallez <abevallez@gmail.com>
2022-01-05 23:29:56 +0100

    refactor in index.ts

---
+++ tests/CreateCardForm.testnono.ts
@@ -0,0 +1,39 @@
+describe('tests CreateCardForm', () => {
+    
+    test('Should fill title with title of first sticker selected"', async () => {
+    const cardExpected = {
+      type: "CARD",
+      title: stickerWidgetMocked.text,
+      description: "<ul><li>" + stickerWidgetMocked.text + "</li></ul>"
+    };
+    miroSDKMock.board.selection.get.mockResolvedValue([stickerWidgetMocked]);
+
+    await cardCreator.createCard();
+    expect(miroSDKMock.board.widgets.create).toBeCalledWith(cardExpected);
+  });
+
+  test('Should fill description with stickers selected as bullet points', async() => {
+    const secondStickerWidgetMocked: SDK.IStickerWidget = mockStickerWidget();
+    const description = "<ul><li>" + stickerWidgetMocked.text + "</li>" 
+    + "<li>" + secondStickerWidgetMocked.text + "</li></ul>" 
+    const cardExpected = {
+    type: "CARD",
+    title: "sticker content",
+    description: description
+    };
+    miroSDKMock.board.selection.get.mockResolvedValue([
+        stickerWidgetMocked, 
+        secondStickerWidgetMocked
+    ]);
+
+    await cardCreator.createCard();
+    expect(miroSDKMock.board.widgets.create).toBeCalledWith(cardExpected);
+  })
+
+  test("Should create a card when input", async () => {
+    miroSDKMock.board.selection.get.mockResolvedValue([stickerWidgetMocked]);
+
+    await cardCreator.createCard();
+    expect(miroSDKMock.board.widgets.create).toBeCalledTimes(1);
+  });
+})
